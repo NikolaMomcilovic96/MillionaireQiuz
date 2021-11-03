f7baea7e3b98651cd17715fecc3ccd4c363ed637
NikolaMomcilovic96 <nikolamomca96@gmail.com>
2021-11-03 18:25:55 +0100

    Started adding helps

--- app/src/main/java/com/raywenderlich/milioner/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/milioner/MainActivity.kt
@@ -17,20 +17,42 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        val fiftyFifty = true
+        val audience = true
+        val friend = true
         val round = 0
         val price = 0
 
-        nextLevel(round, price)
+        nextLevel(round, price, fiftyFifty, audience, friend)
     }
 
     @SuppressLint("SetTextI18n")
-    private fun nextLevel(round: Int, price: Int) {
+    private fun nextLevel(
+        round: Int,
+        price: Int,
+        fiftyFifty: Boolean,
+        audience: Boolean,
+        friend: Boolean,
+    ) {
         var thisRound = round
         var thisPrice = price
+        var thisFiftyFifty = fiftyFifty
+        var thisAudience = audience
+        var thisFriend = friend
 
         binding.questionNumberTextView.text = "Question ${thisRound + 1}"
         binding.questionTextView.text = questions[thisRound].question
         binding.priceTextView.text = "Current reward: $thisPrice $"
+
+        if (fiftyFifty) {
+            binding.halfHalfButton.isEnabled = true
+        }
+        if (audience) {
+            binding.audienceButton.isEnabled = true
+        }
+        if (friend) {
+            binding.friendButton.isEnabled = true
+        }
 
         val answers = arrayListOf(questions[thisRound].rightAnswer,
             questions[thisRound].wrong1,
@@ -50,22 +72,49 @@ class MainActivity : AppCompatActivity() {
         binding.textViewD.text = answer4
         answers.remove(answer4)
 
+        fun fiftyFiftyHelp() {
+            binding.halfHalfButton.isEnabled = false
+            thisFiftyFifty = false
+        }
+
+        fun askAudienceHelp() {
+            binding.audienceButton.isEnabled = false
+            thisAudience = false
+        }
+
+        fun callFriendHelp() {
+            binding.friendButton.isEnabled = false
+            thisFriend = false
+        }
+
         fun checkAnswer(answer: String) {
             if (answer == questions[thisRound].rightAnswer) {
                 Toast.makeText(this, "CORRECT!", Toast.LENGTH_SHORT).show()
                 thisPrice += questions[thisRound].price
                 thisRound += 1
                 if (thisRound < 15) {
-                    nextLevel(thisRound, thisPrice)
+                    nextLevel(thisRound, thisPrice, thisFiftyFifty, thisAudience, thisFriend)
                 } else {
                     Toast.makeText(this, "YOU WON! CONGRATULATIONS!", Toast.LENGTH_SHORT).show()
-                    nextLevel(0, 0)
+                    nextLevel(0, 0, fiftyFifty = true, audience = true, friend = true)
                 }
             } else {
                 Toast.makeText(this, "Wrong answer! Better luck next time!", Toast.LENGTH_SHORT)
                     .show()
-                nextLevel(0, 0)
+                nextLevel(0, 0, fiftyFifty = true, audience = true, friend = true)
             }
+        }
+
+        binding.halfHalfButton.setOnClickListener {
+            fiftyFiftyHelp()
+        }
+
+        binding.friendButton.setOnClickListener {
+            callFriendHelp()
+        }
+
+        binding.audienceButton.setOnClickListener {
+            askAudienceHelp()
         }
 
         binding.textViewA.setOnClickListener {
